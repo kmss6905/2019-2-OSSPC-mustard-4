71d02dda2adf741a3c9725d634252e257d6a78b7
Hankyo2273 <55426284+Hankyo2273@users.noreply.github.com>
2019-11-28 03:54:49 +0900

    Update TetrisBoard.java

--- Tetris_Millions/src/com/tetris/window/TetrisBoard.java
+++ Tetris_Millions/src/com/tetris/window/TetrisBoard.java
@@ -399,8 +399,8 @@ public class TetrisBoard extends JPanel implements Runnable, KeyListener, MouseL
 		ghost = getBlockClone(shap, true);
 		hold = null;
 		isHold = false;
-		controller = new TetrisController(shap, maxX - 1, maxY - 1, map, minX, minY);
-		controllerGhost = new TetrisController(ghost, maxX - 1, maxY - 1, map, minX, minY);
+		controller = new TetrisController(shap, maxX - 1, maxY - 1, map, minX-1, minY-1);
+		controllerGhost = new TetrisController(ghost, maxX - 1, maxY - 1, map, minX-1, minY-1);
 		this.showGhost();
 		for (int i = 0; i < 5; i++) {
 			nextBlocks.add(getRandomTetrisBlock());
