165d9466c3ee4d2f89d16ea85fb45348e0240f83
Minshik kim <kmss69052@naver.com>
2019-12-06 23:35:53 +0900

    edit server login file

--- server/php-react/login-user.php
+++ server/php-react/login-user.php
@@ -3,10 +3,33 @@
 $conn = mysqli_connect("localhost", "min", "alstlrdl1!", "react_php_crud");
 if($conn){}else{exit();} // db 접속실패시 접속 끊음
 $json_array = array();
-$json_array = array(
-    "success" => "ok"
-);
-echo json_encode($json_array, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+
+$id = $_POST['id'];
+$pwd = $_POST['pwd'];
+
+$sql = "select * from react_php_crud.users where user_id = '$id' and user_pwd = '$pwd'";
+if($result = mysqli_query($conn, $sql)){
+    if($num = mysqli_num_rows($result) == 1){
+        $json_array = array(
+            "success" => "ok"
+        );
+
+        echo json_encode($json_array, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+        exit;
+    }else{
+        $json_array = array(
+            "success" => "fail"
+        );
+
+        echo json_encode($json_array, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+        exit;
+    }
+}
+
+
+
+
+
 
 
 ?>
